1595e9668030816cb9a2b665a3cc204d2d9b24d4
Ponharish <e1123017@u.nus.edu>
2024-10-06 15:54:41 +0800

    Update AddressBookParser.java

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -71,7 +71,7 @@ public class AddressBookParser {
             return new HelpCommand();
 
         case RemarkCommand.COMMAND_WORD:
-            return new RemarkCommand();
+            return new RemarkCommandParser().parse(arguments);
 
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
